2a5ebd34456038706b30cc23c133e3ed00c51e51
Kartik-khandelwal-git <kartikkhandelwal930@gmail.com>
2025-09-13 15:22:44 +0530

    lasstt streaatchh

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
+useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log("MongoDB connected ✅"))
