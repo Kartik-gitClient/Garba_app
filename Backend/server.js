6e458228439bf49b97496cce5fca627d4f8b8544
Kartik-khandelwal-git <kartikkhandelwal930@gmail.com>
2025-09-13 14:06:51 +0530

    mongo DB url specified

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/garba_event", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log("MongoDB connected ✅"))
+.catch(err => console.log("MongoDB connection error ❌", err));
 
 const SerialSchema = new mongoose.Schema({
   SNo: Number,
